624153da7674d4e9c3176f544bc4e0f7b96d2376
Levi Li <453321481@qq.com>
2024-02-07 02:30:09 +0800

    菜单动画 样式 基本定型

--- src/view/engine/DraggableBubble.tsx
+++ src/view/engine/DraggableBubble.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useRef, useEffect} from 'react';
 import styles from './styles/DraggableBubble.module.scss';
 import {DragOutlined} from "@ant-design/icons";
+import Icon2 from '@/assets/logo/321.svg';
 
 interface BubbleRef {
     offsetX: number;
@@ -34,7 +35,10 @@ const DraggableBubble: React.FC = () => {
     const handleDragEnd = () => {
         setIsDragging(false);
     };
-
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+        // 这里可以添加动画效果的逻辑
+    };
     useEffect(() => {
         if (isDragging) {
             document.addEventListener('mousemove', handleDragMove);
@@ -57,22 +61,50 @@ const DraggableBubble: React.FC = () => {
                 style={{
                     left: `${bubblePosition.x}px`,
                     top: `${bubblePosition.y}px`,
-                    transition: isDragging ? 'none' : 'all 0.3s ease',
+                    transition: isDragging ? 'none' : 'all 0.5s ease',
+
                 }}
-                className={`${styles.bubble} ${isExpanded ? styles.expanded : ''}`}
-                onClick={() => setIsExpanded(!isExpanded)}
+                onClick={() => {
+                    if (!isExpanded) toggleExpand();
+                }}
+                className={isExpanded ? styles.expanded : styles.bubble}
+                onMouseDown={handleDragStart}
             >
-                菜单
+                {isExpanded ? <div className={styles.expandedContent}>
+
+                        <div  className={styles.icon}>
+                           调试流程
+                        </div>
+                        <div onClick={toggleExpand} className={styles.icon}>
+                            <img src={Icon2} alt="icon" style={{width: '42px', height: '42px'}}/>
+                        </div>
+                        <div className={styles.icon}>
+                           更新流程
+                        </div>
+                        <div className={`${styles.icon} ${ styles.iconMove1}`}>
+                           Review 文档
+                        </div>
+                        <div className={styles.icon}>
+                            相关 链接
+                        </div>
+                        <div className={styles.icon}>
+                            6
+                        </div>
+                    </div>
+
+                    : "菜单"}
 
             </div>
             <DragOutlined className={styles.dragIcon} onMouseDown={handleDragStart}
+
                           style={{
                               transition: isDragging ? 'none' : 'all 0.3s ease',
                               position: 'absolute',
                               cursor: 'grab',
-                              left: `${isExpanded ?bubblePosition.x -2: bubblePosition.x + 20}px`,
-                              top: `${isExpanded ? bubblePosition.y - 5:bubblePosition.y - 15}px`,
+                              left: `${isExpanded ? bubblePosition.x - 2 : bubblePosition.x + 20}px`,
+                              top: `${isExpanded ? bubblePosition.y - 5 : bubblePosition.y - 15}px`,
                           }}/>
+
         </>
 
     );
