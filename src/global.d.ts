7c9f2049240b70983a244313bf55833120d2321f
Levi Li <453321481@qq.com>
2024-02-11 18:29:52 +0800

    API

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,6 +5,7 @@ export {}; // 确保 TypeScript 把这个文件当作一个模块
 declare global {
     interface Window {
         compileScriptApiPath: string;
+        executeWorkflowApiPath: string;
         createWorkflowApiPath: string;
         deleteWorkflowApiPath: string;
         updateWorkflowApiPath: string;
